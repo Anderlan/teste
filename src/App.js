aa310687582175130a6ed69518ee690956b33231
anderlan <anderlansiqueira@gmail.com>
2023-10-17 15:56:50 -0300

    modified:   src/pages/home/index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Faq from "./pages/faq";
@@ -8,15 +8,10 @@ import Faq from "./pages/faq";
 function App() {
   return (
     <BrowserRouter>
-      {/* <Link to="/Teste">Home</Link>
-      <Link to="/Faq">FAQ</Link> */}
-
       <Routes>
         <Route exact path="/"  element={<Home />} />
         <Route path="/Faq" element={<Faq />} />
       </Routes>
-      <Link to="/Faq">FAQ</Link>
-      
     </BrowserRouter>
   );
 }
--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Faq = () => {
     return (
         <div>
-            FAQ
+           <Link to='/Faq'>FAQ</Link>
         </div>
     );
 }
